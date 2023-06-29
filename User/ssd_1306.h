a21b7b3fda2be788e5c56789e42b658fceb823e0
Flogger69 <vik22@abv.bg>
2023-06-30 00:12:57 +0300

    Added README file

---
+++ User/ssd_1306.h
@@ -0,0 +1,109 @@
+//#include <stddef.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include "debug.h"
+#include "font.h"
+//#include "ssd1306_fonts.h"
+//#define SSD1306_INCLUDE_FONT_11x18
+//FontDef Font_11x18;
+
+// Enumeration for screen colors
+#ifdef SSD1306_X_OFFSET
+#define SSD1306_X_OFFSET_LOWER (SSD1306_X_OFFSET & 0x0F)
+#define SSD1306_X_OFFSET_UPPER ((SSD1306_X_OFFSET >> 4) & 0x07)
+#else
+#define SSD1306_X_OFFSET_LOWER 0
+#define SSD1306_X_OFFSET_UPPER 0
+#endif
+
+typedef enum {
+    Black = 0x00, // Black color, no pixel
+    White = 0x01  // Pixel is set. Color depends on OLED
+} SSD1306_COLOR;
+
+typedef enum {
+    SSD1306_OK = 0x00,
+    SSD1306_ERR = 0x01  // Generic error.
+} SSD1306_Error_t;
+
+//FontDef_t font1;
+// Struct to store transformations
+typedef struct {
+    uint16_t CurrentX;
+    uint16_t CurrentY;
+    uint8_t Initialized;
+    uint8_t DisplayOn;
+} SSD1306_t;
+
+typedef struct {
+    uint8_t x;
+    uint8_t y;
+} SSD1306_VERTEX;
+
+
+
+
+// SSD1306 OLED height in pixels
+#ifndef SSD1306_HEIGHT
+#define SSD1306_HEIGHT          64
+#endif
+
+// SSD1306 width in pixels
+#ifndef SSD1306_WIDTH
+#define SSD1306_WIDTH           128
+#endif
+
+#ifndef SSD1306_BUFFER_SIZE
+#define SSD1306_BUFFER_SIZE   SSD1306_WIDTH * SSD1306_HEIGHT / 8
+#endif
+
+
+// Procedure definitions
+void ssd1306_Init(void);
+void ssd1306_Fill(SSD1306_COLOR color);
+void ssd1306_UpdateScreen(void);
+void ssd1306_DrawPixel(uint8_t x, uint8_t y, SSD1306_COLOR color);
+char SSD1306_Putc(char ch, FontDef_t* Font, SSD1306_COLOR color);
+//char ssd1306_WriteChar(char ch, FontDef t, SSD1306_COLOR color);
+char ssd1306_WriteString(char* str, FontDef_t, SSD1306_COLOR color);
+char SSD1306_Puts(char* str, FontDef_t* Font, SSD1306_COLOR color);
+void ssd1306_SetCursor(uint8_t x, uint8_t y);
+void ssd1306_Line(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, SSD1306_COLOR color);
+/*void ssd1306_DrawArc(uint8_t x, uint8_t y, uint8_t radius, uint16_t start_angle, uint16_t sweep, SSD1306_COLOR color);
+void ssd1306_DrawArcWithRadiusLine(uint8_t x, uint8_t y, uint8_t radius, uint16_t start_angle, uint16_t sweep, SSD1306_COLOR color);
+//void ssd1306_DrawCircle(uint8_t par_x, uint8_t par_y, uint8_t par_r, SSD1306_COLOR color);
+void ssd1306_FillCircle(uint8_t par_x,uint8_t par_y,uint8_t par_r,SSD1306_COLOR par_color);
+void ssd1306_Polyline(const SSD1306_VERTEX *par_vertex, uint16_t par_size, SSD1306_COLOR color);
+void ssd1306_DrawRectangle(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, SSD1306_COLOR color);
+*/
+void SSD1306_DrawBitmap(int16_t x, int16_t y, const unsigned char* bitmap, int16_t w, int16_t h, uint16_t color);
+void ssd1306_FillRectangle(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, SSD1306_COLOR color);
+/*void ssd1306_DrawBitmap(uint8_t x, uint8_t y, const unsigned char* bitmap, uint8_t w, uint8_t h, SSD1306_COLOR color);
+*/
+void SSD1306_GotoXY(uint16_t x, uint16_t y);
+/**
+ * @brief Sets the contrast of the display.
+ * @param[in] value contrast to set.
+ * @note Contrast increases as the value increases.
+ * @note RESET = 7Fh.
+ */
+void ssd1306_SetContrast(const uint8_t value);
+/**
+ * @brief Set Display ON/OFF.
+ * @param[in] on 0 for OFF, any for ON.
+ */
+void ssd1306_SetDisplayOn(const uint8_t on);
+/**
+ * @brief Reads DisplayOn state.
+ * @return  0: OFF.
+ *          1: ON.
+ */
+uint8_t ssd1306_GetDisplayOn();
+
+// Low-level procedures
+void ssd1306_Reset(void);
+void ssd1306_WriteCommand(u8 byte);
+void ssd1306_WriteData(uint8_t buffer[], size_t buff_size);
+//SSD1306_Error_t ssd1306_FillBuffer(uint8_t* buf, uint32_t len);
+
